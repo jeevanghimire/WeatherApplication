c64984e51156e1d449a3b7454ea1d75fecaa850a
Jeevan Ghimire <imjeevanghimire@gmail.com>
2024-08-28 15:33:11 +0545

    Initial Commit

---
+++ WeatherApplication/WeatherApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApplicationApp.swift
+//  WeatherApplication
+//
+//  Created by Jeevan Ghimire on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
